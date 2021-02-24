c26067120aa8dc4ad272ac5d0b234d46e6221a29
Radu M <root@radu.sh>
2021-02-24 14:51:29 -0800

    Add test component
    Signed-off-by: Radu M <root@radu.sh>

---
+++ src/app/xyz/xyz.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as brigade from '@brigadecore/brigade-sdk';
+
+@Component({
+  selector: 'app-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.css'],
+})
+export class XyzComponent implements OnInit {
+  apiAddress = '';
+  rootPassword = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  async getToken() {
+    let client = new brigade.APIClient(this.apiAddress, '', {
+      allowInsecureConnections: true,
+    });
+
+    let token = await client
+      .authn()
+      .sessions()
+      .createRootSession(this.rootPassword);
+    console.log(token);
+  }
+}
